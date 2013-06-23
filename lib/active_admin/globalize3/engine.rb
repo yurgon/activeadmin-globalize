5398c5a33d06199764a77c396fba103e4454df60
Sebastian Gassner <sebastian.gassner@gmail.com>
2013-06-23 14:51:08 +0200

    Register a before_filter in ActionController::Base to a) remove empty unsaved translations from params and b) mark empty saved translations for deletion.

--- lib/active_admin/globalize3/engine.rb
+++ lib/active_admin/globalize3/engine.rb
@@ -1,3 +1,5 @@
+require 'active_admin/globalize3/filter_empty_translations'
+
 module ActiveAdmin
   module Globalize3
     class Engine < ::Rails::Engine
@@ -11,6 +13,14 @@ module ActiveAdmin
       initializer "add assets" do
         ActiveAdmin.application.register_stylesheet "active_admin/active_admin_globalize3.css", :media => :screen
         ActiveAdmin.application.register_javascript "active_admin/active_admin_globalize3.js"
+      end
+
+      # Register a before_filter in ActionController that will filter out
+      # empty translations submitted by activeadmin-globalize3.
+      # See ActiveAdmin::Globalize3::FilterEmptyTranslations#filter_empty_translations
+      initializer "activeadmin-globalize3.load_helpers" do |app|
+        ActionController::Base.send :include, ActiveAdmin::Globalize3::FilterEmptyTranslations
+        ActionController::Base.send :before_filter, :filter_empty_translations, only: [:create, :update]
       end
 
     end
