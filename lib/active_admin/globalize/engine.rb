de4b2f19cae1e00bdf6985cdacb10dfaa4116679
David Tengeri <dtengeri@inf.u-szeged.hu>
2014-01-27 11:52:52 +0100

    Changed group of asset precompile from assets to all.

--- lib/active_admin/globalize/engine.rb
+++ lib/active_admin/globalize/engine.rb
@@ -1,7 +1,7 @@
 module ActiveAdmin
   module Globalize
     class Engine < ::Rails::Engine
-      initializer "Active Admin precompile hook", group: :assets do |app|
+      initializer "Active Admin precompile hook", group: :all do |app|
         app.config.assets.precompile += [
           "active_admin/active_admin_globalize.css",
           "active_admin/active_admin_globalize.js"
