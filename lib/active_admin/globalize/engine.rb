b40ba3e42150e252920371416cf5a9aec50df4dc
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-09 13:22:52 +0200

    Removed not needed filter

--- lib/active_admin/globalize/engine.rb
+++ lib/active_admin/globalize/engine.rb
@@ -1,5 +1,3 @@
-require 'active_admin/globalize3/filter_empty_translations'
-
 module ActiveAdmin
   module Globalize
     class Engine < ::Rails::Engine
@@ -13,14 +11,6 @@ module ActiveAdmin
       initializer "add assets" do
         ActiveAdmin.application.register_stylesheet "active_admin/active_admin_globalize.css", :media => :screen
         ActiveAdmin.application.register_javascript "active_admin/active_admin_globalize.js"
-      end
-
-      # Register a before_filter in ActionController that will filter out
-      # empty translations submitted by activeadmin-globalize3.
-      # See ActiveAdmin::Globalize3::FilterEmptyTranslations#filter_empty_translations
-      initializer "activeadmin-globalize3.load_helpers" do |app|
-        ActionController::Base.send :include, ActiveAdmin::Globalize3::FilterEmptyTranslations
-        ActionController::Base.send :before_filter, :filter_empty_translations, only: [:create, :update]
       end
 
     end
