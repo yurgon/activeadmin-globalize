90b45ef5402ee21a6d235eebd7adf687c321774a
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-11 16:51:04 +0200

    Body is a text field

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(:version => 20140611144346) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
