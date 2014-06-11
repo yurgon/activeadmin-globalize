ccee52c814958deecd942d52594dff5f8072532a
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-11 18:16:49 +0200

    Setup for article translation

--- spec/dummy/app/admin/articles.rb
+++ spec/dummy/app/admin/articles.rb
@@ -1,3 +1,31 @@
 ActiveAdmin.register Article do
 
+  index do
+    id_column
+    column :title
+    translation_status
+    translation_status_flags
+    actions
+  end
+
+  show do
+    attributes_table do
+      translated_row(:title)
+      translated_row(:italian_title, locale: :it, field: :title)
+      translated_row(:body, inline: false)
+    end
+  end
+
+  form do |f|
+
+    f.inputs 'Article details' do
+      f.translated_inputs do |t|
+        t.input :title
+        t.input :body
+      end
+    end
+
+    f.actions
+  end
+
 end
