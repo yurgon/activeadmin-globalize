5719a82684271574437e6801621242aa7cc595d1
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-11 18:03:23 +0200

    Factory for localized article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,4 +7,19 @@ describe Article do
     Article.translates?.should be_true
   end
 
+  describe 'localized article' do
+
+    let(:article) { create(:localized_article) }
+    subject { article }
+
+    it { should have(2).translations }
+
+    it 'should have italian translation' do
+      I18n.with_locale :it do
+        article.title.should == 'Italian title'
+      end
+    end
+
+  end
+
 end
--- spec/support/factories/articles.rb
+++ spec/support/factories/articles.rb
@@ -4,5 +4,14 @@ FactoryGirl.define do
   factory :article do
     title 'Article title'
     body 'Article Body'
+
+    factory :localized_article do
+
+      after :create do |a|
+        I18n.with_locale(:it) { a.update_attributes! title: 'Italian title', body: 'Italian Body' }
+      end
+
+    end
+
   end
 end
