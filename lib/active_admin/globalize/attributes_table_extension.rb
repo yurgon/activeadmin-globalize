4e700734000401a8daf6811c3ca89e0ca286647f
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-12 16:09:48 +0200

    Consistent (fixed) order for translations in show

--- lib/active_admin/globalize/attributes_table_extension.rb
+++ lib/active_admin/globalize/attributes_table_extension.rb
@@ -83,7 +83,7 @@ module ActiveAdmin
       # @param [Symbol] tag tag to enclose field translation
       # @param [Symbol] initial_locale locale to set as not hidden
       def field_translations(field, tag, initial_locale)
-        @record.translations.map do |translation|
+        available_translations.map do |translation|
           # Classes for translation span only first element is visible
           css_classes = ['field-translation', "locale-#{translation.locale}"]
           # Initially only element for selected locale is visible
@@ -98,7 +98,7 @@ module ActiveAdmin
 
       def block_locale_selectors
         content_tag(:ul, class: 'available-locales locale-selector') do
-          @record.translations.map(&:locale).map do |locale|
+          available_translations.map(&:locale).map do |locale|
             default = 'default' if locale == I18n.default_locale
             content_tag(:li, class: 'translation-tab') do
               I18n.with_locale(locale) do
@@ -112,11 +112,15 @@ module ActiveAdmin
       # Return flag elements to show the given locale using javascript
       def inline_locale_selectors
         content_tag(:span, class: 'inline-locale-selector') do
-          @record.translations.map do |translation|
+          available_translations.map do |translation|
             # Build a link to show the given translation
             link_to(flag_icon(translation.locale), '#', class: 'ui-translation-trigger', data: {locale: translation.locale})
           end.join(' ').html_safe
         end
+      end
+
+      def available_translations
+        @record_translations ||= @record.translations.order(:locale)
       end
 
     end
