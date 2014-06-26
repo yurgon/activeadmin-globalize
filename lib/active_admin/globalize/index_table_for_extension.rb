630811c2fddd78d79d647bee19136a45ca774fcb
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-06-26 21:49:59 +0200

    Avoid n+1 query issue when eager_loading is used

--- lib/active_admin/globalize/index_table_for_extension.rb
+++ lib/active_admin/globalize/index_table_for_extension.rb
@@ -12,7 +12,7 @@ module ActiveAdmin
       end
       def translation_status_flags
         column I18n.t("active_admin.globalize.translations") do |obj|
-          obj.translated_locales.map { |l| flag_icon(l) }.join(' ').html_safe
+          obj.translations.map(&:locale).sort.map { |l| flag_icon(l) }.join(' ').html_safe
         end
       end
     end
