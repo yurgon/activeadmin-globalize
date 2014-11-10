fd38348a35aa2372c2979dba939969534e10efa2
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-11-10 18:17:39 +0100

    form_buffers is not needed anymore, closes #3

--- lib/active_admin/globalize/form_builder_extension.rb
+++ lib/active_admin/globalize/form_builder_extension.rb
@@ -7,7 +7,7 @@ module ActiveAdmin
         options.symbolize_keys!
         switch_locale = options.fetch(:switch_locale, false)
         auto_sort = options.fetch(:auto_sort, true)
-        form_buffers.last << template.content_tag(:div, class: "activeadmin-translations") do
+        template.content_tag(:div, class: "activeadmin-translations") do
           template.content_tag(:ul, class: "available-locales") do
             (auto_sort ? I18n.available_locales.sort : I18n.available_locales).map do |locale|
               default = 'default' if locale == I18n.default_locale
